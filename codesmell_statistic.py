49bd3c3155438979ff97169ef3d70f753aaa6721
call203 <call203@daum.net>
2024-07-11 16:03:00 +0100

    extract top 20 rules

--- codesmell_statistic.py
+++ codesmell_statistic.py
@@ -4,30 +4,30 @@ from dotenv import load_dotenv
 import json
 
 load_dotenv()
-# 안되는 파일들 -> 나중에 체크!
-# {'name' :'react', 'key':os.getenv('REACT')},
-# {'name' :'nodejs', 'key':os.getenv('NODEJS')},
+
 projects = [
-    {'name' :'Express', 'key':os.getenv('EXPRESS')},
-            {'name' :'bower', 'key':os.getenv('BOWER')},
-            {'name' :'less', 'key':os.getenv('LESS')},
-            {'name' :'request', 'key':os.getenv('REQUEST')},
-            {'name' :'grunt', 'key':os.getenv('GRUNT')},
-            {'name' :'jquery', 'key':os.getenv('JQUERY')},
-            {'name' :'vuejs', 'key':os.getenv('VUEJS')},
-            {'name' :'ramda', 'key':os.getenv('RAMDA')},
-            {'name' :'reaflet', 'key':os.getenv('LEAFLET')},
-            {'name' :'hexo', 'key':os.getenv('HEXO')},
-            {'name' :'webpack', 'key':os.getenv('WEBPACK')},
-            {'name' :'webtorrent', 'key':os.getenv('WEBTORRENT')},
-            {'name' :'moment', 'key':os.getenv('MOMENT')},
-            {'name' :'riot', 'key':os.getenv('RIOT')},
-            {'name' :'d3', 'key':os.getenv('D3')},
-            {'name' :'lodash', 'key':os.getenv('LODASH')},
-            {'name' :'redux', 'key':os.getenv('REDUX')},
-            {'name' :'axios', 'key':os.getenv('AXIOS')},
-            {'name' :'chartjs', 'key':os.getenv('CHARTJS')}
-            ]
+        {'name' :'Express', 'key':os.getenv('EXPRESS')},
+        {'name' :'bower', 'key':os.getenv('BOWER')},
+        {'name' :'less', 'key':os.getenv('LESS')},
+        {'name' :'request', 'key':os.getenv('REQUEST')},
+        {'name' :'grunt', 'key':os.getenv('GRUNT')},
+        {'name' :'jquery', 'key':os.getenv('JQUERY')},
+        {'name' :'vuejs', 'key':os.getenv('VUEJS')},
+        {'name' :'ramda', 'key':os.getenv('RAMDA')},
+        {'name' :'reaflet', 'key':os.getenv('LEAFLET')},
+        {'name' :'hexo', 'key':os.getenv('HEXO')},
+        {'name' :'webpack', 'key':os.getenv('WEBPACK')},
+        {'name' :'webtorrent', 'key':os.getenv('WEBTORRENT')},
+        {'name' :'moment', 'key':os.getenv('MOMENT')},
+        {'name' :'riot', 'key':os.getenv('RIOT')},
+        {'name' :'d3', 'key':os.getenv('D3')},
+        {'name' :'lodash', 'key':os.getenv('LODASH')},
+        {'name' :'redux', 'key':os.getenv('REDUX')},
+        {'name' :'axios', 'key':os.getenv('AXIOS')},
+        {'name' :'nodejs', 'key':os.getenv('NODEJS')},
+        {'name' :'chartjs', 'key':os.getenv('CHARTJS')},
+        {'name' :'react', 'key':os.getenv('REACT')},
+ ]
 
 serverties="MAJOR,CRITICAL"
 sonarqube_url ='http://localhost:9000/api/issues/search'
@@ -43,8 +43,11 @@ def request_sonarQube(name, key):
         res = requests.get('http://localhost:9000/api/issues/search?'+'severities=' + serverties+ '&components='+ name +'&issueStatuses=CONFIRMED%2COPEN&types=CODE_SMELL&ps=500&p='+str(page),
                             headers={'Authorization' : 'Bearer ' + str(key)})
         page+=1
-        issue_list = res.json()['issues']
-            # No more pages
+        try:
+            issue_list = res.json()['issues']
+        except KeyError:
+             break
+
         if(len(issue_list)==0):
             break
         else: 
@@ -89,17 +92,17 @@ def total_code_smells():
 
         res = request_sonarQube(project['name'],project['key'])
         for issue in res:
-            if issue['message'] not in code_smell_types:
-                code_smell_types[issue['message']] = 1
+            if issue['rule'] not in code_smell_types:
+                code_smell_types[issue['rule']] = 1
             else:
-                code_smell_types[issue['message']]= code_smell_types[issue['message']]+1
+                code_smell_types[issue['rule']]= code_smell_types[issue['rule']]+1
 
-    sorted_smells = dict(sorted(code_smell_types.items(), key=lambda item: item[1], reverse=True)[:20])
-    with open("code_smells.json",'w') as f:
+    sorted_smells = dict(sorted(code_smell_types.items(), key=lambda item: item[1], reverse=True)[:23])
+    with open("top_20_code_smells.json",'w') as f:
         json.dump(sorted_smells,f)
 
 
-get_each_project_smells()
+total_code_smells()
 
 
 
